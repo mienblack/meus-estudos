53cc01588f5bef4018c8ebdbda86b3a992f2a84d
mienblack <damienfelipe2015@gmail.com>
2022-11-01 23:46:35 -0300

    react - complete

--- Programador br/34. React js/my-app/src/App.js
+++ Programador br/34. React js/my-app/src/App.js
@@ -2,15 +2,41 @@ import logo from './logo.svg';
 import Header from './Header'
 import Counter from './Counter'
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <Header name='React Page' links={["Sobre", "Compras", "Contato", "Redes"]}></Header>
-      <Counter count={21}></Counter>
-    </div>
 
-  );
+  const [show,setShow] = useState(true)
+  let time = 0
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      time++
+      console.log(time)
+      if (time > 5) {
+        clearInterval(timer)
+        setShow(false)
+      }
+    }, 1000)
+  }, [])
+
+  if (show) {
+    return (
+      <div>
+        <Header name='React Page' links={["Sobre", "Compras", "Contato", "Redes"]}></Header>
+        <Counter count={0}></Counter>
+      </div>
+
+    )
+  } else {
+    return (
+      <div>
+        <Header name='React Page' links={["Sobre", "Compras", "Contato", "Redes"]}></Header>
+        No Counter Here
+      </div>
+
+    )
+  }
 }
 
 export default App;
