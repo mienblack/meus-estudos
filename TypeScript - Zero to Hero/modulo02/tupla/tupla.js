4c7bf716fdc67542638cae9514c116e3f924d6b9
mienblack <damienfelipe2015@gmail.com>
2023-02-06 12:31:42 -0300

    tipos

---
+++ TypeScript - Zero to Hero/modulo02/tupla/tupla.js
@@ -0,0 +1,9 @@
+"use strict";
+// Uso simples de tupla
+let pessoa;
+pessoa = ['Damien Costa', 'Dev Front-end', 20];
+console.log(pessoa);
+// Acessando o valor da tupla
+console.log(pessoa[1]);
+// lableled tuple
+console.log();
---
+++ TypeScript - Zero to Hero/modulo02/tupla/tupla.ts
@@ -0,0 +1,11 @@
+// Uso simples de tupla
+let pessoa: [string, string, number];
+pessoa = ['Damien Costa', 'Dev Front-end', 20];
+
+console.log(pessoa);
+
+// Acessando o valor da tupla
+console.log(pessoa[1]);
+
+// lableled tuple
+console.log();
